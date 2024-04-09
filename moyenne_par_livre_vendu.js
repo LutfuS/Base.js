df520691839d0eaea8d6617e843895cc2de65f7b
LutfuS <us.lutfu25@gmail.com>
2024-04-09 09:42:22 +0200

    update exo livre vendu

--- moyenne_par_livre_vendu.js
+++ moyenne_par_livre_vendu.js
@@ -1,4 +1,5 @@
-let TotalLivre = 650;
-let LivreAjouter = 50;
-let LivreVenduParJour = (TotalLivre + LivreAjouter) /7;
-console.log(LivreVenduParJour);
+let TotalLivre = 600;                                           //Total des livrs avant ajout
+let LivreAjouter = 50;                                          //Livre Ajouter
+let LivreVenduParJour = (TotalLivre + LivreAjouter) /7;         //Livre vendu par jours
+const message = "Le nombre de livres vendu par jour est:  "   // Const nombre de livre vendu
+console.log(message + Math.round(LivreVenduParJour));                                 // Constater le resultat au bout de 7j
