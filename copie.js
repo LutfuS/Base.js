0d7990af8003803cbad533583fd1d6a514e0f2d6
LutfuS <us.lutfu25@gmail.com>
2024-04-09 15:46:59 +0200

    Copie js

---
+++ copie.js
@@ -0,0 +1,16 @@
+let variableSimple1 = 12
+let variableSimple2 = variableSimple1
+
+variableSimple1= 35
+console.log("variableSimple1", variableSimple1);
+console.log("variableSimple2", variableSimple2);
+
+let variableComplexe1 = ["l'informatique", "sport", "television"]
+let variableComplexe2 = variableComplexe1
+let variableComplexe3 = [...variableComplexe1]
+
+variableComplexe2.push("course")
+
+console.log("variableComplexe1", variableComplexe1)
+console.log("variableComplexe2", variableComplexe2)
+console.log("variableComplexe3", variableComplexe3)
